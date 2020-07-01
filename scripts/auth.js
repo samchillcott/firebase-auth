9fcf28d50532d1f78cafacbc3e5cdef9e50c4726
Sam Chillcott <sam@samchillcott.com>
2020-07-01 11:16:27 +0100

    signout function added

--- scripts/auth.js
+++ scripts/auth.js
@@ -12,10 +12,18 @@ signupForm.addEventListener("submit", (e) => {
 
 	// sign up user
 	auth.createUserWithEmailAndPassword(email, password).then((cred) => {
-		console.log(cred.user);
-
 		const modal = document.querySelector("modal-signup");
 		// M.Modal.getInstance(modal).close();
 		signupForm.reset();
+	});
+});
+
+// log out
+
+const logout = document.querySelector("#logout");
+logout.addEventListener("click", (e) => {
+	e.preventDefault();
+	auth.signOut().then(() => {
+		console.log("User signed out");
 	});
 });
